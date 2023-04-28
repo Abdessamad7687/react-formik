0328937fccfd32ef678654153075c598989bf621
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-04-28 23:40:22 +0100

    react-formik

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,26 @@ import React from 'react'
 import Form from './components/Form'
 const App = () => {
   return (
-    <div className="grid max-w-screen-xl grid-cols-1 gap-8 px-8 py-16 mx-auto rounded-lg md:grid-cols-2 md:px-12 lg:px-16 xl:px-32 dark:bg-gray-800 dark:text-gray-100">
-      <div className="flex flex-col justify-between">
-        <div className="space-y-2">
-          <h2 className="text-4xl font-bold leading-tight lg:text-5xl">Let's talk!</h2>
-          <div className="dark:text-gray-400">Vivamus in nisl metus? Phasellus.</div>
-        </div>
-        <img src="assets/svg/doodle.svg" alt="" className="p-6 h-52 md:h-64" />
+    <section className="p-6 dark:text-gray-100">
+    <form novalidate="" className="container w-full max-w-xl p-8 mx-auto space-y-6 rounded-md shadow dark:bg-gray-900 ng-untouched ng-pristine ng-valid ng-submitted">
+      <h2 className="w-full text-3xl font-bold leading-tight">Contact us</h2>
+      <div>
+        <label for="name" className="block mb-1 ml-1">Name</label>
+        <input id="name" type="text" placeholder="Your name" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 dark:bg-gray-800" />
       </div>
-      <Form />
-    </div>
+      <div>
+        <label for="email" className="block mb-1 ml-1">Email</label>
+        <input id="email" type="email" placeholder="Your email" required="" className="block w-full p-2 rounded focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 dark:bg-gray-800" />
+      </div>
+      <div>
+        <label for="message" className="block mb-1 ml-1">Message</label>
+        <textarea id="message" type="text" placeholder="Message..." className="block w-full p-2 rounded autoexpand focus:outline-none focus:ring focus:ring-opacity-25 focus:ring-violet-400 dark:bg-gray-800"></textarea>
+      </div>
+      <div>
+        <button type="submit" className="w-full px-4 py-2 font-bold rounded shadow focus:outline-none focus:ring hover:ring focus:ring-opacity-50 dark:bg-violet-400 focus:ring-violet-400 hover:ring-violet-400 dark:text-gray-900">Send</button>
+      </div>
+    </form>
+  </section>
   )
 }
 
