b0dff592352f4736292dccc9594e61e0bae08c9b
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-05-06 00:23:48 +0100

    react-formik

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
-import Form from './components/Form'
 import './App.css'
 const App = () => {
   return (
-    <section className="p-6 dark:text-gray-100">
-      <Form />
-    </section>
+    <div className="container mt-5">
+            <form className="form" id="form" autoComplete='off'>
+                
+                <div className="form-group col-md-6 mx-auto mt-3">
+                <label htmlFor="Username">Username</label>
+                    <input type="text" className="form-control" />
+                </div>
+
+                <div className="form-group col-md-6 mx-auto mt-3">
+                <label htmlFor="Email">Email</label>
+                    <input type="email" className="form-control" />
+                </div>
+
+                <div className="form-group col-md-6 mx-auto mt-3">
+                <label htmlFor="Phone">Phone</label>
+                    <input type="number" className="form-control" />  
+                </div>
+
+
+                <div className="form-group col-md-4 mx-auto mt-3">
+                    <button className="btn text-white w-100">
+                        Submit new Person
+                    </button>
+                </div>
+            </form>
+        </div>
   )
 }
 
