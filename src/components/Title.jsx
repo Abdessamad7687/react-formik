9af2163fe4ecacc6ac3c9fc7812b4e00fc086f62
Abdessamad7687 <abdessamadahmali@gmail.com>
2023-05-06 00:27:07 +0100

    react-formik

--- src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Title = ({title}) => {
-  return (
-    <h2 className="w-full m-auto text-3xl font-bold leading-tight">{title}</h2>
-  )
-}
-
-export default Title
